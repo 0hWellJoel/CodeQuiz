c1aec1a8928e941d09aec1f29a48a1af6c525d3c
0hWellJoel <61248738+0hWellJoel@users.noreply.github.com>
2020-08-19 00:34:31 -0400

    updates

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,7 @@
-// variables to keep track of quiz state
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
+var currentriddlesIndex = 0;
+var time = riddles.length * 15;
 var timerId;
-
-// variables to reference DOM elements
-var questionsEl = document.getElementById("questions");
+var riddlesEl = document.getElementById("riddles");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
 var submitBtn = document.getElementById("submit");
@@ -12,159 +9,109 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
-// sound effects
-// var sfxRight = new Audio("assets/sfx/correct.wav");
-// var sfxWrong = new Audio("assets/sfx/incorrect.wav");
-
 function startQuiz() {
-  // hide start screen
   var startScreenEl = document.getElementById("start-screen");
   startScreenEl.setAttribute("class", "hide");
-
-  // un-hide questions section
-  questionsEl.removeAttribute("class");
-
-  // start timer
+  riddlesEl.removeAttribute("class");
   timerId = setInterval(clockTick, 1000);
-
-  // show starting time
   timerEl.textContent = time;
-
-  getQuestion();
+  getriddles();
 }
 
-function getQuestion() {
-  // get current question object from array
-  var currentQuestion = questions[currentQuestionIndex];
-
-  // update title with current question
-  var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
-
-  // clear out any old question choices
+function getriddles() {
+  var currentriddles = riddles[currentriddlesIndex];
+  var titleEl = document.getElementById("riddles-title");
+  titleEl.textContent = currentriddles.title;
   choicesEl.innerHTML = "";
 
-  // loop over choices
-  currentQuestion.choices.forEach(function(choice, i) {
-    // create new button for each choice
+  currentriddles.choices.forEach(function(choice, i) {
+
     var choiceNode = document.createElement("button");
     choiceNode.setAttribute("class", "choice");
     choiceNode.setAttribute("value", choice);
 
     choiceNode.textContent = i + 1 + ". " + choice;
-
-    // attach click event listener to each choice
-    choiceNode.onclick = questionClick;
-
-    // display on the page
+    choiceNode.onclick = riddlesClick;
     choicesEl.appendChild(choiceNode);
   });
 }
 
-function questionClick() {
-  // check if user guessed wrong
-  if (this.value !== questions[currentQuestionIndex].answer) {
-    // penalize time
+function riddlesClick() {
+  if (this.value !== riddles[currentriddlesIndex].answer) {
+    // take time off time
     time -= 15;
 
     if (time < 0) {
       time = 0;
     }
-
-    // display new time on page
     timerEl.textContent = time;
-
-    // play "wrong" sound effect
-    // sfxWrong.play();
 
     feedbackEl.textContent = "Wrong!";
   } else {
-    // play "right" sound effect
-    // sfxRight.play();
 
     feedbackEl.textContent = "Correct!";
   }
-
-  // flash right/wrong feedback on page for half a second
   feedbackEl.setAttribute("class", "feedback");
   setTimeout(function() {
     feedbackEl.setAttribute("class", "feedback hide");
   }, 1000);
 
-  // move to next question
-  currentQuestionIndex++;
-
-  // check if we've run out of questions
-  if (currentQuestionIndex === questions.length) {
+  currentriddlesIndex++;
+  if (currentriddlesIndex === riddles.length) {
     quizEnd();
   } else {
-    getQuestion();
+    getriddles();
   }
 }
 
 function quizEnd() {
   // stop timer
   clearInterval(timerId);
-
-  // show end screen
+  // end screen
   var endScreenEl = document.getElementById("end-screen");
   endScreenEl.removeAttribute("class");
-
-  // show final score
+  //final score
   var finalScoreEl = document.getElementById("final-score");
   finalScoreEl.textContent = time;
-
-  // hide questions section
-  questionsEl.setAttribute("class", "hide");
+  //hide riddles section
+  riddlesEl.setAttribute("class", "hide");
 }
 
 function clockTick() {
   // update time
   time--;
   timerEl.textContent = time;
-
-  // check if user ran out of time
+  // see if user ran out of time
   if (time <= 0) {
     quizEnd();
   }
 }
 
 function saveHighscore() {
-  // get value of input box
   var initials = initialsEl.value.trim();
-
-  // make sure value wasn't empty
   if (initials !== "") {
-    // get saved scores from localstorage, or if not any, set to empty array
     var highscores =
       JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    // format new score object for current user
     var newScore = {
       score: time,
       initials: initials
     };
 
-    // save to localstorage
     highscores.push(newScore);
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
-    // redirect to next page
     window.location.href = "highscores.html";
   }
 }
 
 function checkForEnter(event) {
-  // "13" represents the enter key
+
   if (event.key === "Enter") {
     saveHighscore();
   }
 }
 
-// user clicks button to submit initials
 submitBtn.onclick = saveHighscore;
-
-// user clicks button to start quiz
 startBtn.onclick = startQuiz;
-
 initialsEl.onkeyup = checkForEnter;
---
+++ assets/js/riddles.js
@@ -0,0 +1,29 @@
+var riddles = [
+  {
+    title: "What can you catch but not throw?",
+    choices: ["strings", "a cold", "football", "air"],
+    answer: "a cold"
+  },
+  {
+    title: "What has to be broken before you can use it?",
+    choices: ["eggs", "hammer", "shoes", "bricks"],
+    answer: "eggs"
+  },
+  {
+    title: "What month of the year has 28 days?",
+    choices: ["Only February", "Only March", "Only July", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "What is full of holes but still holds water?",
+    choices: ["Fork", "Sponge", "Hair",],
+    answer: "Sponge"
+  },
+  {
+    title:
+      "What is always in front of you but can’t be seen?",
+    choices: ["John Cena", "Glass", "Water", "The Future"],
+    answer: "The Future"
+  }
+];
